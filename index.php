8af4fc053fd61759d7f8633bb208e894fa26e0a5
yochaiglik <yochai@epalle.co.il>
2024-07-21 01:28:01 +0300

    add - seasons colors with light and dark schemas

--- index.php
+++ index.php
@@ -29,14 +29,55 @@
     </title>
 
     <style>
+
+        /* custom properties declaration */
+        :root {
+
+            --summer-color: oklch(0.5 0.1 142.49); /* nature green */
+            --autumn-color: oklch(0.5 0.1 49.29); /* fall brown orange */
+            --winter-color: oklch(0.5 0.1 236.54); /* cold ice blue */
+            --spring-color: oklch(0.5 0.1 350.31); /* blossom pink */
+
+
+            @media (prefers-color-scheme: dark) {
+                --summer-color: oklch(0.65 0.3 142.49); /* nature green */
+                --autumn-color: oklch(0.65 0.3 49.29); /* fall brown orange */
+                --winter-color: oklch(0.65 0.3 236.54); /* cold ice blue */
+                --spring-color: oklch(0.65 0.3 350.31); /* blossom pink */
+
+            }
+
+            --season-color: var(--summer-color);
+
+        }
+
         html {
             font-family: system-ui;
+
+            color: black;
+            background-color: white;
+
+            @media (prefers-color-scheme: dark) {
+                color: white;
+                background-color: black;;
+            }
         }
 
         p {
             max-width: 60ch;
             line-height: 1.5;
         }
+
+        h1, h2, h3, h4, h5, h6 {
+            color: var(--season-color);
+        }
+
+        a {
+            @media (prefers-color-scheme: dark) {
+                color: rgb(158, 158, 255); /* webkit-link on dark color scheme */
+            }
+        }
+
     </style>
 
 </head>
